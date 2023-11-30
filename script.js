db78bb75bafee6f6c4afcab0688a5f66fabf79ce
Ulyt1703 <138807803+Ulyt1703@users.noreply.github.com>
2023-11-30 21:17:00 +0200

    D__push__30.11.2023

---
+++ script.js
@@ -0,0 +1,16 @@
+let result = 5 + 5 + '5'
+console.log(typeof result)
+
+let email = 'bogdansergovoj@gmail.com'
+console.log(email.length)
+console.log(email.includes('@'))
+
+let my = 'my'
+let names = 'name' 
+let is = 'is'
+let fullName = `${my } ${names } ${is } Victor`
+console.log(fullName)
+
+let userName = 'Victor'
+let payment = '300'
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`)
